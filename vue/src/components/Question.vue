194af5d79e5996f4f700ca22ae28be780bf4848d
ATaylorN <amandanagies@yahoo.com>
2023-09-03 21:23:42 -0400

    pushing

--- vue/src/components/Question.vue
+++ vue/src/components/Question.vue
@@ -1,10 +1,39 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{randomQuestion.question}}</p>
+    <p>{{randomQuestion.answer}}</p>
+
+  </div>
 </template>
 
 <script>
-export default {
+import QuestionService from '../services/QuestionService'
 
+export default {
+ name: 'questions',
+ data() {
+  return {
+    randomQuestion: {
+      id: '',
+      question: '',
+      answer: ''
+    }
+  }
+ },
+ created: {
+  getQuestion() {
+    QuestionService.getQuestion().then((response) => {
+      if(response.status === 200){
+        this.randomQuestion.id = response.data.id;
+        this.randomQuestion.question = response.data.question;
+        this.randomQuestion.answer = response.data.answer;
+      }
+    })
+    .catch (error => {
+      console.error(error);
+    })
+  }
+ }
 }
 </script>
 
