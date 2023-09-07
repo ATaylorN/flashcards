587da1f7680592a6b76d72be6e76216435699469
ATaylorN <amandanagies@yahoo.com>
2023-09-07 16:58:50 -0400

    pushing studymode updates

--- vue/src/components/StudyMode.vue
+++ vue/src/components/StudyMode.vue
@@ -3,6 +3,9 @@
     <button @click="getQuestion()">Start</button>
     <button @click="getNextQuestion()">Next Question</button>
 
+    <button @click="markCorrect()">Correct</button>
+    <button @click="markIncorrect()">Incorrect</button>
+
     <div >
       <p>{{ randomQuestion.question }}</p>
     </div>
@@ -55,13 +58,30 @@ export default {
     },
     getNextQuestion() {
       QuestionService.getQuestionById(this.randomQuestion.id + 1).then((response) => {
-        if(response.status === 200){
+        if(response.data.id != null){
             this.randomQuestion.id = response.data.id;
             this.randomQuestion.question = response.data.question;
             this.randomQuestion.answer = response.data.answer;
             this.showAnswer = false;
         }
+        else {
+            this.randomQuestion.id = 0;
+        }
       })
+    },
+    markCorrect(){
+        if(this.incorrect.includes(this.randomQuestion.id)){
+            this.incorrect.pop(this.randomQuestion.id);
+        }
+        this.correctlyAnswered.push(this.randomQuestion.id);
+        this.getNextQuestion();
+    },
+    markIncorrect(){
+        if(this.correctlyAnswered.includes(this.randomQuestion.id)){
+            this.correctlyAnswered.pop(this.randomQuestion.id)
+        }
+        this.incorrect.push(this.randomQuestion.id);
+        this.getNextQuestion();
     },
     toggleShowAnswer(){
         this.showAnswer = !this.showAnswer;
