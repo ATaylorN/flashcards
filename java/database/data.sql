dded31cae21299c6e753fd2b7524d9e72fb7ffe3
ATaylorN <amandanagies@yahoo.com>
2023-09-25 23:42:04 -0400

    more questions and answers

--- java/database/data.sql
+++ java/database/data.sql
@@ -99,7 +99,89 @@ INSERT INTO questions (question, answer)
 VALUES ('What are the two forms of the INSERT statement in SQL? Which is preferred and why?',
 		'The two forms of the INSERT statement in SQL are: INSERT INTO table_name VALUES (value1, value2,...) and INSERT into table_name (column1, column2,...) VALUES (value1, value2,...). The second version is preferred because it allows for clarity by specifying column names and enables insertion into specific columns even if you do not have values for all of the columns.');
 
+INSERT INTO questions (question, answer)
+VALUES ('Why would a developer want to develop and maintain a SQL script for a database under development or being changed?',
+		'A developer would maintain a SQL script for a few reasons: Reproducibility - Scripts can recreate the database schema or data whenever needed which is useful for testing or deploying to new enviornments. Version Control - Script allows tracking changes over time and enables reverting to a previous state if necessary. Collaboration - Scripts make it easier for team members to understand database structure and apply consistent changes. Automation - Database creation or modification can be automated using scripts which reduces manual errors.');
 
+INSERT INTO questions (question, answer)
+VALUES ('What is a DAO and what OOP principal does it most clearly express?',
+		'A DAO, or Data Access Object, is a design pattern that provides an abstract interface for accessing data from different sources like databases or APIs. A DAO most clearly expresses the principle of encapsulation by hiding the complexity of data access operations from the rest of the application, providing a simpler interface to interact with data sources.');
+
+INSERT INTO questions (question, answer)
+VALUES ('Why is it important to use parameter substitution rather than string concatenation when building SQL strings inside of code?',
+		'Using parameter substitution rather than string concatenation prevents SQL Injection attacks which occur when malicious SQL code is inserted into a query. Parameter substitution ensures input is treated as literal data rather than as executable code.');
+
+INSERT INTO questions (question, answer)
+VALUES ('What software vulnerability is totally under the control of the programmer?',
+		'Hardcoding sensitive information - a software vulnerability that is under the control of the programmer, even a junior developer. This includes embedding passwords, encryption keys, or API keys directly into the source code. This practice is a security risk and is easily avoidable by using secure methods of configuration management and enviornment variables.');
+
+INSERT INTO questions (question, answer)
+VALUES ('What are some examples of Integration Testing?',
+		'Integration Testing involves testing how different components of a system work together. Examples include: Testing client-server communication - Checking if the client and server components interact correctly. Testing database integration - Verifying the applications interaction with the database. Testing web services - Checking how your application interacts with external services. Testing UI with backend - Ensuring the user interface works correctly with the backend logic. Testing API endpoints - Making sure different APIs interact as expected when used in combination.');
+
+INSERT INTO questions (question, answer)
+VALUES ('Why would you want to use SQL to select the specific information you need inside your program? Why not just select all the information from a table and hold it in a collection inside your program?',
+		'Using SQL to select specific information is more efficient for two main reasons: Network Efficiency - it reduces the amount of data sent over the network which speeds up data transfer times and Memory Efficiency - it minimizes memory usage in your application by only holding necessary data. Selecting all the data can lead to performance issues, especially with large datasets. It also increases the risk of exposing sensitive information.');
+
+INSERT INTO questions (question, answer)
+VALUES ('What does the first forward slash in a URL represent? What does the question mark in the URL represent?',
+		'In a URL, the first forward slash after the domain nname is used to seperate the domain from the path to a specific resource or page within the website. The question mark in a URL is used to start the query string which typically includes parameters to be sent to a server-side script. It seperates the path from the parameters.');
+
+INSERT INTO questions (question, answer)
+VALUES ('In HTTP, how are the GET and POST verbs alike? How are they different?',
+		'GET and POST are HTTP methods used to request data from a server. Similarities include: they can both send requests to the server, they are both used in the HTTP request-response process. Differences include: GET appends data to the URL (in the query string) while POST sends data in the body of the HTTP request, GET is idempotent (repeated requests have the same effect as a single one) while POST is not, GET requests can be cached and bookmarked while POST requests cannot, GET has large restrictions while POST does not.');
+
+INSERT INTO questions (question, answer)
+VALUES ('Assume four blocks of code: 1) Controller and 2) Model-DAOs for the server and 3) User Interface and 4) API Service for a client. Describe the flow of a REST request and response. Describe the role-responsibility of each block of code.',
+		'Here is the flow of a REST request and response: User Interface(Client) - The user interacts with the UI which triggers a REST request to the server (ex. to get data). API Service(Client) - The API service processes this request, formats it correctly (often as an HTTP request) and send it to the server. Controller(Server) - The servers controller receives the request, determines what the client is asking for, and directs the request to the appropriate model or DAO for processing. Model-DAOs(Server) - The Model or DAO interacts with the servers database or data source to carry out the request. It retrieves or manipulates the data as needed. The data or result is snet back to the Controller which then formats it into a suitable HTTP response. This response is sent back to the API service on the client side. API Service(Client) - The API service on the client receives this response, processes the data as necessary (like converting from JSON to JavaScript objects) and sends it to the UT. User Interface(Client) - The UI receives this data and uses it to update the interface, completing the request-response cycle. Each block of code has its own responsibilities, seperating concerns and making the system more maintainable.)');
+		
+INSERT INTO questions (question, answer)
+VALUES ('How can we test that our client-server application is following the rules and protocols of REST?',
+		'To test if a client-server application follows REST principles, you can: 1) Use tools like Postman or curl to send requests to your server and examine the responses. 2) Verify that they use standard HTTP methods (GET, POST, PUT, DELETE), have meaningful status codes, and return data in a stateless manner. Ensure resources are identified by URLS and that the application adheres to a logical, consistent URL structure. 3) Check that the responses are in a standard format like JSON or XML. 4) Ensure the application is stateless (i.e. server responses are independent of previous requests). 5) Test for idempotency, meaning multiple identical requests have the same effect as a single one. 6) Use automated testing frameworks to continuously test these aspects. These measures will help confirm that your application is abiding by REST conventions.');
+
+INSERT INTO questions (question, answer)
+VALUES ('What is a JWT? How is it generated and how is it used?',
+		'JWT, or JSON Web Token, is a compact, URL-safe means of representing claims to be transferred between two parties. It is used for authentication and secure information exchange. A JWT is generated by: 1) Creating a header with token type and the hashing algorithm. 2) Creating a payload with the claims (like user details or permissions). 3) Encoding the header and payload as JSON and then BaseURL encoding. 4) Concatenating these with a dot seperator and signing this string using the specified algorithm and a secret key. In use, when a user logs in, the server generates a JWT and sends it back. For subsequent requests, the users client attaches this JWT often in the Authorization header. The server can then validate the token and verify the users identity and permissions. This allows for stateless, sessionless authentication.');
+
+INSERT INTO questions (question, answer)
+VALUES ('Describe MVC in the context of a Web Service running on a server.',
+		'MVC, or Model-View-Controller, is a design pattern often used in webservices: 1) Model - this represents the data and the rules that govern access to and updates of this data. In a webservice, models interact with the database, handling create, read, update, and delete(CRUD) operations. 2) View - in the context of a web service, is the format and structure of the output sent to the client. This could be JSON, XML, or HTML depending on the service. It is how the data from the model is presented. 3) Controller - the intermediary between the Model and the View. It receives client requests, interacts with the Modal to get or manipulate data, and then formats this data witht eh View to send as a response. In MVC, each component has a distinct responsibility, making the web service easier to maintain, test, and scale.');
+
+INSERT INTO questions (question, answer)
+VALUES ('Why do HTTP POST and PUT use a request body? Why not just put information in the path or query string of the URL?',
+		'HTTP POST and PUT use a request body primarily for two reasons: 1) Data Security - sensitive information is safer in the request body than in the URL as URLs can be logged or cached. 2) Data Size - HTTP methods that use the request body can send much larger amounts of data than those using a URL. URLs have length limitations. Using the request body allows sending of complex data types, like nested JSON, which would not be feasible in a URL. Thus, for operations that involve sending large or sensitive data, methods with request bodies are preferred.');
+
+INSERT INTO questions (question, answer)
+VALUES ('What should HTTP status codes in the 200s indicate? The 400s? The 500s? What specific HTTP status codes do you know and what should they mean?',
+		'HTTP status codes indicate the result of the HTTP request: 200s - These are successful response codes (ex. 200 OK - the request was successful), 201 Created - The request has been fulfilled and resulted in a new resource being created, 204 No Content - The server successfully processed the request and is not returning any content. 400s - These are client error response codes (ex. 400 Bad Request - The server could not understand the request due to invalid syntax, 401 Unauthorized - The request requires user authorization, 404 Not Found - The server has not found anything matching the Request-URI). 500s - Internal Server Error: the server encountered an unexpected condition which prevented it from fulfilling the request (ex. 503 Service Unavailable - The server is currently unable to handle the request due to a temporary overloading or maintenance). These codes help clients understand the servers response to their requests.');
+
+INSERT INTO questions (question, answer)
+VALUES ('In JavaScript, should you use let or var to create variables? Why?',
+		'In modern JavaScript, it is recommended to use let or const over var to declare variables. This is because let and const have block scope, meaning they only exist within the block they are in. Var has function scope which can lead to unexpected behavior. Let allows you to declare variables that can be reassigned while const allows you to declare variables that are constant or unchangeable. Car is still valid JavaScript, but its usage can lead to confusion, especially for those coming from other languages where block scoping is standard.');
+
+INSERT INTO questions (question, answer)
+VALUES ('In a world of HTML, CSS, and JavaScript, what role do each provide in the browser?',
+		'HTML, or HyperText Markup Language, is the structure of the webpage. It creates and organizes content in the form of text, images, and more. CSS, or Cascading Style Sheets, is the design of the webpage. It determines how the HTML elements are displayed, including colors, layouts, fonts, and animations. JavaScript is the behavior of the webpage. It provides interactivity allowing elements on the page to respond to user actions like clicks, input, and form submission. Together they create dynamic, interactive webpages. HTML provides the base content, CSS makes it visually appealing, and JavaScript adds user interaction.');
+
+INSERT INTO questions (question, answer)
+VALUES ('What are some common selectors used with HTML and CSS and how are they specified in CSS?',
+		'Tag or Element selector - applies style to all elements of a specifed type. It is simply the name of the HTML tag. Class selector - applies styles to all elements with the specified class. It is denoted by a period followed by the class name. ID selector - applies styles to the element with the specified ID. It is denoted by the hash followed by the ID. Note: an ID should be unique within a page while classes can be applied to multiple elements.');
+
+INSERT INTO questions (question, answer)
+VALUES ('In CSS, what does it mean when multiple selectors are used together with spaces between them? With no spaces between them? With commas between them? With the greater than symbol between them?',
+		'Multiple selectors with spaces is a descendant selector. With no spaces, is a class selector for a specific element. With commas between them is a group selector. The greater than symbol is a child selector.');
+
+INSERT INTO questions (question, answer)
+VALUES ('In general, what part of a webpage layout should be controlled by CSS grid? What part should be controlled by CSS flex?',
+		'CSS grid is generally used for defining two-dimensional layouts, handling both columns and rows. It is ideal for larger scale layouts like whole pages or significant page sections. Flex is typically used for one-dimensional layouts, dealing with either a column or a row at a time. It is useful for smaller-scale layouts within a page, like aligning items in a header, footer, or controlling the layout of specific components.');
+
+INSERT INTO questions (question, answer)
+VALUES ('In JavaScript, this symbol (=>) is referred to by what name? What JavaScript keyword does it replace? Bonus: what are some other names that this symbol is called?',
+		'It is called an arrow function and replaces the function keyword when defining a function. Bonus: It is also referred to as a fat arrow, lambda, anonymous function, or closure, but these terms have broader meanings.');
+
+INSERT INTO questions (question, answer)
+VALUES ('The major elements of a SQL SELECT are written in a certain order. What is that order?',
+		'The order in a SQL SELECT statement is: SELECT, FROM, WHERE, GROUP BY, HAVING, ORDER BY. Each clause is optional except for SELECT and FROM.');
 
 
 COMMIT TRANSACTION;
